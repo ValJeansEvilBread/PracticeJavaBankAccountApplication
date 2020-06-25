f5f2b33d2a1c5c6ab5cd41ee7407d3c7e3b06926
ValJeansEvilBread <64847510+ValJeansEvilBread@users.noreply.github.com>
2020-06-25 19:28:04 -0400

    first commit

---
+++ src/Checking.java
@@ -0,0 +1,36 @@
+
+public class Checking extends Account{
+	// List properties specific for a checking account
+	private int debitCardNumber;
+	private int debitCardPIN;
+	
+	// Constructor to initialize checking account properties
+	public Checking(String name, String sSN, double initDeposit) {
+		super(name, sSN, initDeposit);
+		accountNumber = "2" + accountNumber;
+		setDebitCard();
+		
+	}
+	
+	@Override
+	public void setRate() {
+		rate = getBaseRate() * .15;
+	}
+	
+	// List any methods specific to the checking account
+	
+	private void setDebitCard() {
+		debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+		debitCardPIN = (int) (Math.random() * Math.pow(10, 4));
+		
+	}
+	
+	public void showInfo() {
+		super.showInfo();
+		System.out.println("Your Checking Account Features: " +
+				"\n Debit Card Number: " + this.debitCardNumber +
+				"\n Debit Card PIN: " + this.debitCardPIN
+				);
+	}
+
+}
